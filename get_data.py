8b25a37637f0584407c7b1f176a87db324e3a84e
SethDKelly <ollioxenhomefree@gmail.com>
2018-10-03 06:33:47 -0500

    Update csv file to update_ratings

--- get_data.py
+++ get_data.py
@@ -2,7 +2,7 @@ def get_data() :
     
     import pandas as pd
     
-    csv_beer = pd.read_csv("/home/grimoire/Projects/BeerRatings/beer_reviews.csv")
+    csv_beer = pd.read_csv("/home/grimoire/Projects/BeerRatings/rating_update.csv")
     beer_ratings = pd.DataFrame(csv_beer)
     
     return beer_ratings
