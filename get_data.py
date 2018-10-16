7b4a8d6e4aae88200d3218acad3d82d537d241ac
SethDKelly <ollioxenhomefree@gmail.com>
2018-10-16 16:07:05 -0500

    Update comments to reflect changes

--- get_data.py
+++ get_data.py
@@ -3,6 +3,8 @@ def get_data() :
     import pandas as pd
     
     # rating_update is the original data 'beer_review.csv' with beer_style updated
+    # bad brewery and profile names updated
+    # beer_style updated
     # The updates change beer_style to match the styles that are given in 'beer_description.csv'
 
     csv_beer = pd.read_csv("/home/grimoire/Projects/BeerRatings/rating_update.csv")
