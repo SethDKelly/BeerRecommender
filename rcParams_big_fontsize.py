997c0cbcbe0a7de05358e34e1b07c1bb91eae83d
SethDKelly <ollioxenhomefree@gmail.com>
2019-01-18 20:28:34 -0600

    Include way to update rcParams Test gridsearchcv for logistic regression
    Needs to gridsearchcv random forest

---
+++ rcParams_big_fontsize.py
@@ -0,0 +1,7 @@
+from matplotlib import rcParams
+
+rcParams.update({'axes.titlesize'  : 22,
+                 'axes.labelsize'  : 22,
+                 'xtick.labelsize' : 18,
+                 'ytick.labelsize' : 18,
+                 'legend.fontsize' : 18})
